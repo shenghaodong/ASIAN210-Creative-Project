b644b4fdf3f2ecc2350429d6931fef0ac4a169f7
Shenghao <shenghaodong18@gmail.com>
2024-04-16 11:05:30 -0400

    added nav bar and more styling

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
 
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   var panel = this.nextElementSibling;
+   if (panel.style.display === "block") {
+     panel.style.display = "none";
+   } else {
+     panel.style.display = "block";
+   }
+ });
+}
